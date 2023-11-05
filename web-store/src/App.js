8f269f3846049a6d9e71c4aee6347e780cd09a6c
Tumisho Makgeru <makgerutumisho55@gmail.com>
2023-11-05 08:44:19 +0200

    adding routes

--- web-store/src/App.js
+++ web-store/src/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
