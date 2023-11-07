08a2870aeb770891f61849822cd49aa556801745
Tumisho Makgeru <makgerutumisho55@gmail.com>
2023-11-07 11:31:19 +0200

    adding login,checkout, footer

--- web-store/src/App.js
+++ web-store/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Checkout from "./components/Checkout/Checkout";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </div>
   );
--- web-store/src/components/Home/Home.jsx
+++ web-store/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavigationBar from "../Navbar/Navigationbar";
 import Carosel from "../Carosel/Carosel";
+import Footer from "../Footer/Footer";
 import "./Home.css";
 
 function Home() {
@@ -51,6 +52,113 @@ function Home() {
           </div>
         </div>
       </div>
+
+      <div className="container px-4 py-5">
+        <h2 className="pb-2 border-bottom text-light">Features with title</h2>
+        <div className="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
+          <div className="col d-flex flex-column align-items-start gap-2">
+            <h2 className="fw-bold text-body-light">
+              Left-aligned title explaining these awesome features
+            </h2>
+            <p className="text-light">
+              Paragraph of text beneath the heading to explain the heading.
+              We'll add onto it with another sentence and probably just keep
+              going until we run out of words.
+            </p>
+            <a href="#" className="btn btn-primary btn-lg">
+              Primary button
+            </a>
+          </div>
+          <div className="col">
+            <div className="row row-cols-1 row-cols-sm-2 g-4">
+              <div className="col d-flex flex-column gap-2">
+                <div className="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+                  <svg className="bi" width="1em" height="1em">
+                    <use xlinkHref="#collection" />
+                  </svg>
+                </div>
+                <h4 className="fw-semibold mb-0 text-body-emphasis">
+                  Featured title
+                </h4>
+                <p className="text-body-secondary">
+                  Paragraph of text beneath the heading to explain the heading.
+                </p>
+              </div>
+              <div className="col d-flex flex-column gap-2">
+                <div className="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+                  <svg className="bi" width="1em" height="1em">
+                    <use xlinkHref="#gear-fill" />
+                  </svg>
+                </div>
+                <h4 className="fw-semibold mb-0 text-body-emphasis">
+                  Featured title
+                </h4>
+                <p className="text-body-secondary">
+                  Paragraph of text beneath the heading to explain the heading.
+                </p>
+              </div>
+              <div className="col d-flex flex-column gap-2">
+                <div className="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+                  <svg className="bi" width="1em" height="1em">
+                    <use xlinkHref="#speedometer" />
+                  </svg>
+                </div>
+                <h4 className="fw-semibold mb-0 text-body-emphasis">
+                  Featured title
+                </h4>
+                <p className="text-body-secondary">
+                  Paragraph of text beneath the heading to explain the heading.
+                </p>
+              </div>
+              <div className="col d-flex flex-column gap-2">
+                <div className="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3">
+                  <svg className="bi" width="1em" height="1em">
+                    <use xlinkHref="#table" />
+                  </svg>
+                </div>
+                <h4 className="fw-semibold mb-0 text-body-emphasis">
+                  Featured title
+                </h4>
+                <p className="text-body-secondary">
+                  Paragraph of text beneath the heading to explain the heading.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="px-4 py-5 my-5 text-center">
+        <img
+          className="d-block mx-auto mb-4"
+          src="../assets/brand/bootstrap-logo.svg"
+          alt=""
+          width={72}
+          height={57}
+        />
+        <h1 className="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">
+            Quickly design and customize responsive mobile-first sites with
+            Bootstrap, the world’s most popular front-end open source toolkit,
+            featuring Sass variables and mixins, responsive grid system,
+            extensive prebuilt components, and powerful JavaScript plugins.
+          </p>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
+              Primary button
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-secondary btn-lg px-4"
+            >
+              Secondary
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
